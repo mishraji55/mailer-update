eba3b6af1b96d48c50f85905f731cd4ba7033981
Romanch Roshan Singh <codedpool10@gmail.com>
2025-01-08 15:42:01 +0530

    mongo error tackle

--- backend/server.js
+++ backend/server.js
@@ -21,7 +21,7 @@ app.use(cors());
 console.log("MONGODB_URI:", process.env.MONGODB_URI);
 
 // Connect to MongoDB
-mongoose.connect(process.env.MONGODB_URI, { useNewUrlParser: true, useUnifiedTopology: true })
+mongoose.connect(process.env.MONGODB_URI)
   .then(() => console.log("Connected to MongoDB"))
   .catch((err) => {
     console.error("Failed to connect to MongoDB:", err.message);
