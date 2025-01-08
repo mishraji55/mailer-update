df6491f9ee26ebf43cfa671d7a84bf590929edee
Romanch Roshan Singh <codedpool10@gmail.com>
2025-01-08 23:37:50 +0530

    update route

--- backend/server.js
+++ backend/server.js
@@ -102,8 +102,8 @@ app.get(
   "/auth/google/callback",
   passport.authenticate("google", { failureRedirect: "/login" }),
   (req, res) => {
-    // Redirect to the frontend after successful login
-    res.redirect("https://mailer1-d1qw.onrender.com/dashboard");
+    // Redirect to the CampaignForm route after successful login
+    res.redirect("https://mailer1-d1qw.onrender.com/campaign");
   }
 );
 
