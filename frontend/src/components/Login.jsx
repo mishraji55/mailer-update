5fb2c953e0a106d0afb7fc63d1e896ceb536729e
Romanch Roshan Singh <codedpool10@gmail.com>
2025-01-08 23:29:45 +0530

    page change issue

--- frontend/src/components/Login.jsx
+++ frontend/src/components/Login.jsx
@@ -1,16 +1,12 @@
 import React from "react";
 
-
 const BACKEND_URL = "https://mailer-backend-7ay3.onrender.com";
 
-const Login = () => (
+const Login = ({ handleGoogleLogin }) => (
   <div className="login-container">
     <h1>Welcome to Email Sender</h1>
     <p>Please log in with Google to start sending emails.</p>
-    <button
-      onClick={() => (window.location.href = `${BACKEND_URL}/auth/google`)}
-      className="login-button"
-    >
+    <button onClick={handleGoogleLogin} className="login-button">
       Login with Google
     </button>
   </div>
